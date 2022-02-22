86ed9d3074a977a7871aec68567457e3702c25c4
Verpet <petroveronika@seznam.cz>
2022-02-22 17:56:24 +0100

    2 files modified, 1 added, 1 deleted

--- promenne.js
+++ promenne.js
@@ -2,3 +2,6 @@ console.log("tak sakra funguj")
 console.log('no proto')
 console.log('Jaká je odpověď na otázku života, vesmíru a vůbec?');
 console.log('42')
+
+let prsi= true; 
+let snezi= false; 
