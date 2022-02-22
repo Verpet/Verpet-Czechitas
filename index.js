ab17c8ea59410ffa4f3ccbfa18f69af57f9e2301
Verpet <petroveronika@seznam.cz>
2022-02-22 18:21:24 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,6 +2,3 @@
 import './style.css';
 import './promenne.js';
 import './ukol1.js'
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
