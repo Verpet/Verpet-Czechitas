4a17b8cf50241eb0cfc0fd49046d30a1fd9413ea
Verpet <petroveronika@seznam.cz>
2022-02-24 17:05:48 +0100

    1 file modified

---
+++ ukol2.js
@@ -0,0 +1,10 @@
+let cislo = Number (prompt ( 'zadej odlišné číslo odlišné od nuly))
+
+
+while(cislo ===0){
+  cislo = Number ( prompt ( 'zadej cislo odlisne od nuly'))
+}
+
+conslole.log(cislo)
+
+
