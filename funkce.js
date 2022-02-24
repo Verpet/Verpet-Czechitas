1e8503a0e4eaab071366d4d53cb307f8320073f5
Verpet <petroveronika@seznam.cz>
2022-02-24 19:29:23 +0100

    1 file modified

--- funkce.js
+++ funkce.js
@@ -5,13 +5,15 @@ funktion srnp(x,y); {
   let soucin = x*y; 
   let podil = x/y;
 
-  console.log('soucet:' +x'+'+y'='+soucet)
-  console.log('rozdil:' +x'-'+y'='+soucet)
-  console.log('soucin:' +x'*'+y'='+soucet)
-  console.log('podil:' +x'/'+y'='+soucet)
-
-
+  console.log('soucet:' +x'+'+y'='+soucet);
+  console.log('rozdil:' +x'-'+y'='+soucet);
+  console.log('soucin:' +x'*'+y'='+soucet);
+  console.log('podil:' +x'/'+y'='+soucet);
 }
+
+
 
 let x= Number(prompt('zadej X'))
 let x= Number(prompt('zadej y'))
+
+srnp (x,y)
